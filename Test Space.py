f8eec6cfa3c65a9ed517954867c3e0b5c141216e
Ekaterinaelf <ekaterina.elfimova.vlg@gmail.com>
2024-10-10 14:12:38 -0400

    Hello class

--- Test Space.py
+++ Test Space.py
@@ -2,6 +2,8 @@ from time import sleep
 from rich.traceback import install; install()
 print("Hello World")
 print("Mike B was here")
+print("Hello class")
+
 
 def blam(name: str = "?") -> str:
     """
